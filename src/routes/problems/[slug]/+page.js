b848f06acaac532b9f8a8b301ffce2951b801d58
Deyao Chen <chendeyao000@gmail.com>
2022-08-20 08:49:00 +0800

    use raw.githubusercontent instead of jsdeliver

--- src/routes/problems/[slug]/+page.js
+++ src/routes/problems/[slug]/+page.js
@@ -1,8 +1,8 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, fetch }) {
-	const problemUrl = `https://cdn.jsdelivr.net/gh/CP-STA/contest-problems@main/${params.slug}/statement.json`;
+	const problemUrl = `https://raw.githubusercontent.com/CP-STA/contest-problems/main/${params.slug}/statement.json`;
 	const problemResponse = await fetch(problemUrl);
-	const languageUrl = `https://cdn.jsdelivr.net/gh/CP-STA/contest-problems@main/supported-languages.json`;
+	const languageUrl = `https://raw.githubusercontent.com/CP-STA/contest-problems/main/supported-languages.json`;
 	const languageResponse = await fetch(languageUrl);
 	return { problem: await problemResponse.json(), languages: await languageResponse.json() };
 }
